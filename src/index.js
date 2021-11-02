7c9a0ff57ce79b538ac13fa9a25c39833b405195
mausawicki7 <mau.sawicki@gmail.com>
2021-11-02 19:40:11 -0300

    Inicio de REST API

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Configuración del servidor
+
+// Middlewares
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor en puerto 3000');
+});
